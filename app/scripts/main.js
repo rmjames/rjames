d143cc683fd16c466599aa68cd79c17f7c63ea53
gtrmjz@gmail.com <gtrmjz@gmail.com>
2014-05-08 14:57:33 -0400

    scroll transition

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,24 @@
 'use strict';
 
-$('a.nav-toggle').on('click', function(){
-		$('.nav-top').slideToggle('1500');
-	});
+$(document).ready(function() {
 
-/*
-var myScroll;
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 900);
+          return false;
+        }
+      }
+    });
+  });
 
-function loaded () {
-	myScroll = new IScroll('#wrapper', { mouseWheel: true, click: true });
-}
+	$('a.nav-toggle').on('click', function(){
+			$('.nav-top').slideToggle('1500');
+		});
 
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-*/
+});
--- javascripts/main.js
+++
@@ -1,7 +0,0 @@
-$('li#btn').on('click', function(){
-		$('#work').slideToggle('ease');})
-
-$('a.nav-toggle').on('click', function(){
-		$('.nav-hdr').slideToggle('ease');
-})
-
