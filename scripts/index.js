0e9e9f1786ef9745b978aa7358995fba9e4062d6
Robert James <rma.james@gmail.com>
2024-09-11 15:35:11 -0400

    init page transition

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,32 @@ self.addEventListener("load", () => {
 });
 
 console.log("This Portfolio is an open design");
+
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+   const coverElement = document.querySelector('.cover');
+   if (coverElement) {
+      coverElement.style.viewTransitionName = 'cover';
+    }
+
+   if (!document.startViewTransition) {
+     console.warn('View Transitions API is not supported');
+     return;
+   }
+ 
+   document.addEventListener('click', (e) => {
+     if (e.target.matches('a[data-view-transition]')) {
+       e.preventDefault();
+       
+       const link = e.target;
+       const href = e.target.href;
+
+       link.classList.add('link-clicked');
+       
+       document.startViewTransition(() => {
+         window.location = href;
+       });
+     }
+   });
+ });
+ 
