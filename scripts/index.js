a4d577f27aea605f8a48ef02aa1af6a5542323e0
Robert James <rma.james@gmail.com>
2025-09-27 00:29:10 -0400

    Project summary (#64)
    * add popover to brands
    
    * update Google Analyticsto track brand engagement
    
    * clean up
    
    * clean up

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,39 @@
 self.addEventListener("load", () => {
-   if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-         .register("/sw.js")
-         .then(registration =>
-            console.log(
-               "serviceWorker registered in scope: ",
-               registration.scope
-            )
-         )
-         .catch(err => console.log("registration failed :", err));
-   }
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) =>
+        console.log("serviceWorker registered in scope: ", registration.scope),
+      )
+      .catch((err) => console.log("registration failed :", err));
+  }
 });
 
 console.log("This Portfolio is an open design");
 
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const coverElement = document.querySelector(".cover");
+  if (coverElement) {
+    coverElement.style.viewTransitionName = "cover";
+  }
 
-document.addEventListener('DOMContentLoaded', async (e) => {
-   const coverElement = document.querySelector('.cover');
-   if (coverElement) {
-      coverElement.style.viewTransitionName = 'cover';
+  if (!document.startViewTransition) {
+    console.warn("View Transitions API is not supported");
+    return;
+  }
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("a[data-view-transition]")) {
+      e.preventDefault();
+
+      const link = e.target;
+      const href = e.target.href;
+
+      link.classList.add("link-clicked");
+
+      document.startViewTransition(() => {
+        window.location = href;
+      });
     }
-
-   if (!document.startViewTransition) {
-     console.warn('View Transitions API is not supported');
-     return;
-   }
- 
-   document.addEventListener('click', (e) => {
-     if (e.target.matches('a[data-view-transition]')) {
-       e.preventDefault();
-       
-       const link = e.target;
-       const href = e.target.href;
-
-       link.classList.add('link-clicked');
-       
-       document.startViewTransition(() => {
-         window.location = href;
-       });
-     }
-   });
- });
- 
+  });
+});
