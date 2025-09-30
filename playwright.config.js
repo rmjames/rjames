f5f7d288ec38af0fde98673935c5eaaa016aaa9f
Robert James <rma.james@gmail.com>
2025-09-30 17:04:33 -0400

    Update branding content (#69)
    * add additional devices and viewports to playwright
    
    * update content
    
    * adjust popover transition timing

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests-e2e',
+  testDir: "./tests-e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -15,28 +15,86 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://127.0.0.1:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: "chromium-1366",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1366, height: 768 },
+      },
+    },
+    {
+      name: "chromium-1280",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1280, height: 720 },
+      },
+    },
+    {
+      name: "chromium-mobile",
+      use: { ...devices["Pixel 5"], viewport: { width: 393, height: 851 } },
+    },
+    {
+      name: "chromium-tablet",
+      use: {
+        ...devices["Galaxy Tab S4"],
+        viewport: { width: 712, height: 1138 },
+      },
+    },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"], viewport: { width: 393, height: 851 } },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"], viewport: { width: 390, height: 844 } },
+    },
+    {
+      name: "Samsung Galaxy",
+      use: { ...devices["Galaxy S9+"], viewport: { width: 320, height: 658 } },
+    },
+    {
+      name: "iPad",
+      use: { ...devices["iPad Pro"], viewport: { width: 1024, height: 1366 } },
+    },
+    {
+      name: "Android Tablet",
+      use: {
+        ...devices["Galaxy Tab S4"],
+        viewport: { width: 712, height: 1138 },
+      },
     },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    command: "npm run start",
+    url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
