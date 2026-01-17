09bf0ae28dcda7c342ac577e00f5be9079c4a65a
Robert James <rma.james@gmail.com>
2026-01-16 20:17:29 -0500

    Perf: audio library (#157)
    * update manifest shortcuts
    
    * add perf optimizatiom

--- scripts/scan-audio.js
+++ scripts/scan-audio.js
@@ -147,8 +147,13 @@ function findAssetKey(src) {
 
 class AudioLibrary {
     constructor() {
-        const rawTracks = ${JSON.stringify(sortedTracks, null, 4)};
-        this._tracks = rawTracks.map(track => {
+        this._rawTracks = ${JSON.stringify(sortedTracks, null, 4)};
+        this._tracks = null;
+    }
+
+    _init() {
+        if (this._tracks) return;
+        this._tracks = this._rawTracks.map(track => {
             const srcKey = findAssetKey(track.src);
             const srcUrl = srcKey ? audioAssets[srcKey] : null;
 
@@ -169,11 +174,25 @@ class AudioLibrary {
                 albumArt: mappedArt || null
             };
         });
+        this._rawTracks = null; // Cleanup
     }
-    get tracks() { return this._tracks; }
-    getAll() { return this._tracks; }
-    getById(id) { return this._tracks.find(track => track.id === id); }
-    addTrack(track) { this._tracks.push(track); }
+
+    get tracks() { 
+        this._init();
+        return this._tracks; 
+    }
+    getAll() { 
+        this._init();
+        return this._tracks; 
+    }
+    getById(id) { 
+        this._init();
+        return this._tracks.find(track => track.id === id); 
+    }
+    addTrack(track) { 
+        this._init();
+        this._tracks.push(track); 
+    }
 }
 export const audioLibrary = new AudioLibrary();
 `;
